faede321dd69270ee6d6fb8dad3c9833ea47e13d
Stanhoucke <stanhoucke@gmail.com>
2021-02-24 15:30:23 +0000

    canFindWhiskyByAgeAndDistillery passes

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.WhiskyTracker.repositories;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,8 @@ public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
 
     // get all whisky for a particular year
     List<Whisky> findWhiskyByYear(int year);
+
+    // Get all the whisky from a particular distillery that's a specific age
+    List<Whisky> findWhiskyByAgeAndDistillery(int age, Distillery distillery);
 
 }
--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,16 +1,39 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WhiskyTrackerApplicationTests {
 
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void canFindWhiskyByAgeAndDistillery(){
+		Distillery foundDistillery = distilleryRepository.getOne(1L);
+		List<Whisky> foundWhiskies = whiskyRepository.findWhiskyByAgeAndDistillery(15, foundDistillery);
+		assertEquals("The Glendronach Revival", foundWhiskies.get(0).getName());
+	}
+
 
 }
